3c964f9e9e5de74075fa6ef4401db20627d0064e
Fabian Gutierrez <fagossa@gmail.com>
2016-11-03 22:59:58 +0100

    wip: fix test

--- src/main/scala/fr/xebia/streams/transform/ByteStringUtils.scala
+++ src/main/scala/fr/xebia/streams/transform/ByteStringUtils.scala
@@ -1,13 +1,28 @@
 package fr.xebia.streams.transform
 
 import akka.util.ByteString
+import org.slf4j.LoggerFactory
 
 object ByteStringUtils {
 
-  def trimChunk(chunk: ByteString, limit: List[Byte]): ByteString =
+  val logger = LoggerFactory.getLogger(getClass)
+
+  def trimChunk(chunk: ByteString, limit: List[Byte]): ByteString = {
     if (chunk.containsSlice(limit)) {
-      val (_, after) = chunk.splitAt(chunk.indexOfSlice(limit))
-      after
+      if (chunk.toList.startsWith(limit)
+        || chunk.toList.endsWith(limit)) {
+        ByteString(removeSubList[Byte](chunk.toList, limit): _*)
+      } else {
+        val reverse = chunk.reverse
+        val (before, _) = reverse.splitAt(reverse.indexOfSlice(limit.reverse))
+        before.reverse
+      }
     } else chunk
+  }
+
+  def removeSubList[T](list: List[T], subList: List[T]): List[T] =
+    if (list.containsSlice(subList))
+      removeSubList(list.diff(subList), subList)
+    else list
 
 }
